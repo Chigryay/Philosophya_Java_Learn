f5aecbc998bda30d578a173c72dc5489f11a7726
ALex Chiga <Fademon2019@gmail.com>
2023-12-05 15:54:04 +0700

    Added Lesson_1_3

---
+++ Work_Directory/Examples/src/Lesson_1/Lesson_1_3.java
@@ -0,0 +1,10 @@
+package Lesson_1;
+
+public class Lesson_1_3 {
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) {
+            if (i >= 5) break;
+            System.out.println(i);
+        }
+    }
+}
