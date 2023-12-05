39f6a7ef4f1f5d70b829f99d2dccc50889f29b38
ALex Chiga <Fademon2019@gmail.com>
2023-12-05 14:16:46 +0700

    Added Lesson_1_2

---
+++ Work_Directory/Examples/src/Lesson_1/Lesson_1_2.java
@@ -0,0 +1,14 @@
+package Lesson_1;
+
+public class Lesson_1_2 {
+    public static void main(String[] args) {
+        System.out.println(test(0, 10, 4));
+        System.out.println(test(8, 12, 14));
+        System.out.println(test(-1, 10, -2));
+    }
+    static int test(int begin, int end, int testVal) {
+        if (testVal >= begin && testVal <= end) return 0;
+        else if (testVal > end) return 1;
+        else return -1;
+    }
+}
